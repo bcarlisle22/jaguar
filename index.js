dfca1f3dd0a5c2da95fc9f8ae0b9a1c3250f2a80
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2024-02-12 23:45:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,39 @@ function showHide(){
 // function showHide(){
 //     document.querySelector("#dropdown-nav-icon")
 //     .addEventListener("click", function () {
-//         alert("why isn't this working....lol...");
+//         alert("why isn't this working......");
 //     });
 // }
 
+//window prompt - retailers page
+
+// window.prompt("wwww.jaguarusa.com wwants to know your location")
+// add allow or block buttons 
+
+
+// display and hide function for builders link on main navigation 
+
+const buildersBox = document.querySelector(".builds-dropdown");
+let buildersClicked = true;
+
+function displayHide(){
+    if (buildersClicked){
+        buildersBox.style.display = "block";
+        buildersClicked = false;
+    }
+    else{
+        buildersBox.style.display ="none";
+        buildersClicked = true;
+    }
+}
+// hide function for builders link on main navigation - close button
+
+const buildsNav = document.querySelector(".builds-dropdown");
+let closeNavbutton = true;
+
+function closeBuilders(){
+    if (closeNavbutton){
+        buildsNav.style.display ="none";
+        closeNavbutton = true;
+    }
+}
